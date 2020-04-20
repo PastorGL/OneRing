7ecb334f17cc463dc46a071114280effcf043d18
Alexey Evdokimov <a.evdokimov@russianit.ru>
2020-04-20 18:22:48 +0400

    skipLines Operation

---
+++ SimpleFilters/src/main/java/ash/nazg/simplefilters/operations/SkipLinesOperation.java
@@ -0,0 +1,134 @@
+package ash.nazg.simplefilters.operations;
+
+import ash.nazg.config.InvalidConfigValueException;
+import ash.nazg.config.tdl.Description;
+import ash.nazg.config.tdl.TaskDescriptionLanguage;
+import ash.nazg.spark.Operation;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaRDDLike;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+@SuppressWarnings("unused")
+public class SkipLinesOperation extends Operation {
+    @Description("Java regex to define line pattern")
+    public static final String OP_LINE_PATTERN = "line.pattern";
+    @Description("Exact value to define line to skip (used only if a regex pattern isn't set)")
+    public static final String OP_LINE_VALUE = "line.value";
+    @Description("For line pattern, reverse operation to skip non-conforming lines")
+    public static final String OP_REVERSE = "reverse";
+    @Description("By default, skip lines conforming to a pattern. If set to 'true', reverse this condition")
+    public static final Boolean DEF_REVERSE = false;
+    @Description("By default, regex pattern is null")
+    public static final String DEF_LINE_PATTERN = null;
+    @Description("By default, exact skip value is null")
+    public static final String DEF_LINE_VALUE = null;
+
+    public static final String VERB = "skipLines";
+
+    private String inputName;
+    private String outputName;
+    private boolean skip;
+    private boolean byPattern;
+    private String value;
+
+    @Override
+    @Description("Treat input as opaque Text RDD, and skip all lines that conform to a regex pattern (or an exact value)." +
+            " Or, do the opposite for the pattern: skip all non-conforming lines")
+    public String verb() {
+        return VERB;
+    }
+
+    @Override
+    public TaskDescriptionLanguage.Operation description() {
+        return new TaskDescriptionLanguage.Operation(VERB,
+                new TaskDescriptionLanguage.DefBase[]{
+                        new TaskDescriptionLanguage.Definition(OP_REVERSE, Boolean.class, DEF_REVERSE),
+                        new TaskDescriptionLanguage.Definition(OP_LINE_PATTERN, DEF_LINE_PATTERN),
+                        new TaskDescriptionLanguage.Definition(OP_LINE_VALUE, DEF_LINE_VALUE),
+                },
+
+                new TaskDescriptionLanguage.OpStreams(
+                        new TaskDescriptionLanguage.DataStream(
+                                new TaskDescriptionLanguage.StreamType[]{TaskDescriptionLanguage.StreamType.Plain},
+                                false
+                        )
+                ),
+
+                new TaskDescriptionLanguage.OpStreams(
+                        new TaskDescriptionLanguage.DataStream(
+                                new TaskDescriptionLanguage.StreamType[]{TaskDescriptionLanguage.StreamType.Plain},
+                                false
+                        )
+                )
+        );
+    }
+
+    @Override
+    public void configure(Properties config, Properties variables) throws InvalidConfigValueException {
+        super.configure(config, variables);
+
+        inputName = describedProps.inputs.get(0);
+        outputName = describedProps.outputs.get(0);
+
+        Boolean reverse = describedProps.defs.getTyped(OP_REVERSE);
+
+        skip = !reverse;
+        value = describedProps.defs.getTyped(OP_LINE_PATTERN);
+        if (skip && (value == null)) {
+            value = describedProps.defs.getTyped(OP_LINE_VALUE);
+
+            if (value == null) {
+                throw new InvalidConfigValueException("Operation '" + name + "' requires regex pattern or exact value");
+            }
+
+            byPattern = false;
+        } else {
+            if (value == null) {
+                throw new InvalidConfigValueException("Operation '" + name + "' requires a regex pattern");
+            }
+
+            byPattern = true;
+        }
+    }
+
+    @Override
+    public Map<String, JavaRDDLike> getResult(Map<String, JavaRDDLike> input) {
+        boolean _skip = skip;
+        boolean _byPattern = byPattern;
+        String _value = value;
+
+        JavaRDD<Object> output = ((JavaRDD<Object>) input.get(inputName))
+                .mapPartitions(it -> {
+                    List<Object> ret = new ArrayList<>();
+
+                    Pattern pattern = _byPattern ? Pattern.compile(_value) : null;
+
+                    while (it.hasNext()) {
+                        Object o = it.next();
+                        String line = String.valueOf(o);
+
+                        if (_skip) {
+                            if (_byPattern) {
+                                if (!pattern.matcher(line).matches()) {
+                                    ret.add(o);
+                                }
+                            } else {
+                                if (!_value.equals(line)) {
+                                    ret.add(o);
+                                }
+                            }
+                        } else {
+                            if (pattern.matcher(line).matches()) {
+                                ret.add(o);
+                            }
+                        }
+                    }
+
+                    return ret.iterator();
+                });
+
+        return Collections.singletonMap(outputName, output);
+    }
+}
---
+++ SimpleFilters/src/test/java/ash/nazg/simplefilters/SkipLinesOperationTest.java
@@ -0,0 +1,57 @@
+package ash.nazg.simplefilters;
+
+import ash.nazg.spark.TestRunner;
+import org.apache.hadoop.io.Text;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaRDDLike;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class SkipLinesOperationTest {
+    @Test
+    public void skipValueTest() throws Exception {
+        try (TestRunner underTest = new TestRunner("/config.skipValue.properties")) {
+            Map<String, JavaRDDLike> ret = underTest.go();
+
+            List<Text> res = ((JavaRDD<Text>) ret.get("filtered")).collect();
+
+            assertFalse(res.isEmpty());
+            for (Object o : res) {
+                assertNotEquals("ts,group,uid,lat,lon", String.valueOf(o));
+            }
+        }
+    }
+
+    @Test
+    public void skipPatternTest() throws Exception {
+        try (TestRunner underTest = new TestRunner("/config.skipPattern.properties")) {
+            Map<String, JavaRDDLike> ret = underTest.go();
+
+            List<Text> res = ((JavaRDD<Text>) ret.get("filtered")).collect();
+
+            assertFalse(res.isEmpty());
+            for (Object o : res) {
+                assertNotEquals("ts,group,uid,lat,lon", String.valueOf(o));
+                assertFalse(Double.parseDouble(String.valueOf(o).split(",", 5)[3]) < 32.D);
+            }
+        }
+    }
+
+    @Test
+    public void skipPatternReverseTest() throws Exception {
+        try (TestRunner underTest = new TestRunner("/config.skipPatternReverse.properties")) {
+            Map<String, JavaRDDLike> ret = underTest.go();
+
+            List<Text> res = ((JavaRDD<Text>) ret.get("filtered")).collect();
+
+            assertFalse(res.isEmpty());
+            for (Object o : res) {
+                assertTrue(Double.parseDouble(String.valueOf(o).split(",", 5)[3]) < 32.D);
+            }
+        }
+    }
+}
