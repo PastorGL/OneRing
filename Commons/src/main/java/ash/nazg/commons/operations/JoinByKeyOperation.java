9b57bc86a0f676e6802874eeba76e76cd02c0bb0
Alexey Evdokimov <a.evdokimov@russianit.ru>
2020-07-14 17:19:22 +0400

    extend joinByKey to allow multiple joins

--- Commons/src/main/java/ash/nazg/commons/operations/JoinByKeyOperation.java
+++ Commons/src/main/java/ash/nazg/commons/operations/JoinByKeyOperation.java
@@ -8,16 +8,13 @@ import ash.nazg.config.InvalidConfigValueException;
 import ash.nazg.config.tdl.Description;
 import ash.nazg.config.tdl.TaskDescriptionLanguage;
 import ash.nazg.spark.Operation;
-import ash.nazg.config.OperationConfig;
 import com.opencsv.CSVParser;
 import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVWriter;
 import org.apache.hadoop.io.Text;
 import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaRDDLike;
 import org.apache.spark.api.java.Optional;
-import org.spark_project.guava.primitives.Ints;
 import scala.Tuple2;
 
 import java.io.Serializable;
@@ -28,9 +25,9 @@ import java.util.*;
 public class JoinByKeyOperation extends Operation {
     @Description("Type of the join")
     public static final String OP_JOIN = "join";
-    @Description("Default value for the missing right row")
+    @Description("Default value for the missing right row values")
     public static final String OP_DEFAULT_RIGHT = "default.right";
-    @Description("Default value for the missing left row")
+    @Description("Default value for the missing left row values")
     public static final String OP_DEFAULT_LEFT = "default.left";
     @Description("By default, any missing row from left is an empty string")
     public static final String DEF_DEFAULT_LEFT = "";
@@ -41,21 +38,19 @@ public class JoinByKeyOperation extends Operation {
 
     public static final String VERB = "joinByKey";
 
-    private String inputName0;
-    private String inputName1;
-    private char inputDelimiter0;
-    private char inputDelimiter1;
+    private String[] inputNames;
+    private char[] inputDelimiters;
     private String outputName;
-    private int[] outputColumns;
+    private Tuple2<Integer, Integer>[] outputColumns;
     private char outputDelimiter;
 
     private Join joinType;
-    private String inputDefault0;
-    private String inputDefault1;
+    private String inputDefaultL;
+    private String inputDefaultR;
 
     @Override
-    @Description("Takes two PairRDDs and joins them by key. In the terms of joins, first PairRDD is the 'left'," +
-            " second is the 'right'. Missing rows from either PairRDDs are settable and must have a proper column count.")
+    @Description("Takes two or more Pair RDDs and joins them by key. Missing rows from either Pair RDDs on left and" +
+            " right side of the join are settable and must have a proper column count")
     public String verb() {
         return VERB;
     }
@@ -89,95 +84,72 @@ public class JoinByKeyOperation extends Operation {
     public void configure(Properties properties, Properties variables) throws InvalidConfigValueException {
         super.configure(properties, variables);
 
-        inputName0 = describedProps.inputs.get(0);
-        inputName1 = describedProps.inputs.get(1);
-        inputDelimiter0 = dataStreamsProps.inputDelimiter(inputName0);
-        inputDelimiter1 = dataStreamsProps.inputDelimiter(inputName1);
+        inputNames = describedProps.inputs.toArray(new String[0]);
+        if (inputNames.length < 2) {
+            throw new InvalidConfigValueException("There must be at least two Pair RDDs to join by '" + name + "'");
+        }
 
         outputName = describedProps.outputs.get(0);
 
-        Map<String, Integer> input1Columns = dataStreamsProps.inputColumns.get(inputName0);
-        Map<String, Integer> input2Columns = dataStreamsProps.inputColumns.get(inputName1);
+        Map<String, Tuple2<Integer, Integer>> inputColumns = new HashMap<>();
+        inputDelimiters = new char[inputNames.length];
+        for (int i = 0; i < inputNames.length; i++) {
+            inputDelimiters[i] = dataStreamsProps.inputDelimiter(inputNames[i]);
 
-        List<Integer> out = new ArrayList<>();
+            for (Map.Entry<String, Integer> ic : dataStreamsProps.inputColumns.get(inputNames[i]).entrySet()) {
+                inputColumns.put(ic.getKey(), new Tuple2<>(i, ic.getValue()));
+            }
+        }
+
         String[] outColumns = dataStreamsProps.outputColumns.get(outputName);
+        List<Tuple2<Integer, Integer>> out = new ArrayList<>();
         for (String outCol : outColumns) {
-            if (input1Columns.containsKey(outCol)) {
-                out.add(input1Columns.get(outCol));
-            }
-            if (input2Columns.containsKey(outCol)) {
-                out.add(-input2Columns.get(outCol) - 1);
-            }
+            out.add(inputColumns.get(outCol));
         }
 
         joinType = describedProps.defs.getTyped(OP_JOIN);
-        if (joinType == Join.LEFT || joinType == Join.OUTER) {
-            inputDefault1 = describedProps.defs.getTyped(OP_DEFAULT_RIGHT);
-        }
         if (joinType == Join.RIGHT || joinType == Join.OUTER) {
-            inputDefault0 = describedProps.defs.getTyped(OP_DEFAULT_LEFT);
+            inputDefaultL = describedProps.defs.getTyped(OP_DEFAULT_LEFT);
+        }
+        if (joinType == Join.LEFT || joinType == Join.OUTER) {
+            inputDefaultR = describedProps.defs.getTyped(OP_DEFAULT_RIGHT);
         }
 
-        outputColumns = Ints.toArray(out);
+        outputColumns = out.toArray(new Tuple2[0]);
         outputDelimiter = dataStreamsProps.outputDelimiter(outputName);
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public Map<String, JavaRDDLike> getResult(Map<String, JavaRDDLike> input) {
-        final char _inputDelimiter0 = inputDelimiter0;
-        final char _inputDelimiter1 = inputDelimiter1;
+        final char[] _inputDelimiters = inputDelimiters;
         final char _outputDelimiter = outputDelimiter;
-        final String _inputDefault0 = inputDefault0;
-        final String _inputDefault1 = inputDefault1;
-        final int[] _outputColumns = outputColumns;
+        final String _inputDefaultL = inputDefaultL;
+        final String _inputDefaultR = inputDefaultR;
+        final Tuple2<Integer, Integer>[] _outputColumns = outputColumns;
 
-        JavaPairRDD j;
-        JavaPairRDD<Object, Object> leftInputRDD = (JavaPairRDD<Object, Object>) input.get(inputName0);
-        JavaPairRDD<Object, Object> rightInputRDD = (JavaPairRDD<Object, Object>) input.get(inputName1);
+        JavaPairRDD<Object, Object> leftInputRDD = (JavaPairRDD<Object, Object>) input.get(inputNames[0]);
 
-        switch (joinType) {
-            case LEFT:
-                j = leftInputRDD.leftOuterJoin(rightInputRDD);
-                break;
-            case RIGHT:
-                j = leftInputRDD.rightOuterJoin(rightInputRDD);
-                break;
-            case OUTER:
-                j = leftInputRDD.fullOuterJoin(rightInputRDD);
-                break;
-            default:
-                j = leftInputRDD.join(rightInputRDD);
-        }
-
-        JavaRDD out = j.mapPartitions(ito -> {
-            List<Text> res = new ArrayList<>();
-
-            Iterator it = (Iterator) ito;
+        JavaPairRDD<Object, Object> j = leftInputRDD.mapPartitionsToPair(it -> {
+            List<Tuple2<Object, Object>> res = new ArrayList<>();
 
             while (it.hasNext()) {
-                Object o = it.next();
+                Tuple2<Object, Object> o = it.next();
 
-                Tuple2 v = (Tuple2) ((Tuple2) o)._2;
-
-                String l1 = (v._1 instanceof String) ?
-                        (String) v._1 : String.valueOf((v._1 instanceof Optional) ?
-                        ((Optional) v._1).orElse(_inputDefault0) : v._1);
-                CSVParser parser1 = new CSVParserBuilder().withSeparator(_inputDelimiter0).build();
+                String l1 = String.valueOf(o._2);
+                CSVParser parser1 = new CSVParserBuilder().withSeparator(_inputDelimiters[0]).build();
                 String[] line1 = parser1.parseLine(l1);
-
-                String l2 = (v._2 instanceof String) ?
-                        (String) v._2 : String.valueOf((v._2 instanceof Optional) ?
-                        ((Optional) v._2).orElse(_inputDefault1) : v._2);
-                CSVParser parser2 = new CSVParserBuilder().withSeparator(_inputDelimiter1).build();
-                String[] line2 = parser2.parseLine(l2);
 
                 StringWriter buffer = new StringWriter();
 
                 String[] acc = new String[_outputColumns.length];
                 int i = 0;
-                for (Integer col : _outputColumns) {
-                    acc[i++] = (col >= 0) ? line1[col] : line2[-col - 1];
+                for (Tuple2<Integer, Integer> col : _outputColumns) {
+                    if (col._1 == 0) {
+                        acc[i] = line1[col._2];
+                    }
+
+                    i++;
                 }
 
                 CSVWriter writer = new CSVWriter(buffer, _outputDelimiter, CSVWriter.DEFAULT_QUOTE_CHARACTER,
@@ -185,13 +157,80 @@ public class JoinByKeyOperation extends Operation {
                 writer.writeNext(acc, false);
                 writer.close();
 
-                res.add(new Text(buffer.toString()));
+                res.add(new Tuple2<>(o._1, new Text(buffer.toString())));
             }
 
             return res.iterator();
         });
 
-        return Collections.singletonMap(outputName, out);
+        for (int r = 1; r < inputNames.length; r++) {
+            JavaPairRDD<Object, Object> rightInputRDD = (JavaPairRDD<Object, Object>) input.get(inputNames[r]);
+
+            JavaPairRDD jj;
+            switch (joinType) {
+                case LEFT:
+                    jj = j.leftOuterJoin(rightInputRDD);
+                    break;
+                case RIGHT:
+                    jj = j.rightOuterJoin(rightInputRDD);
+                    break;
+                case OUTER:
+                    jj = j.fullOuterJoin(rightInputRDD);
+                    break;
+                default:
+                    jj = j.join(rightInputRDD);
+            }
+
+            final int _r = r;
+            j = jj.mapPartitionsToPair(ito -> {
+                List<Tuple2> res = new ArrayList<>();
+
+                Iterator<Tuple2> it = (Iterator) ito;
+
+                while (it.hasNext()) {
+                    Tuple2<Object, Object> o = it.next();
+
+                    Tuple2<Object, Object> v = (Tuple2<Object, Object>) o._2;
+
+                    String l1 = (v._1 instanceof String) ?
+                            (String) v._1 : String.valueOf((v._1 instanceof Optional) ?
+                            ((Optional) v._1).orElse(_inputDefaultL) : v._1);
+                    CSVParser parser1 = new CSVParserBuilder().withSeparator(_outputDelimiter).build();
+                    String[] line1 = parser1.parseLine(l1);
+
+                    String l2 = (v._2 instanceof String) ?
+                            (String) v._2 : String.valueOf((v._2 instanceof Optional) ?
+                            ((Optional) v._2).orElse(_inputDefaultR) : v._2);
+                    CSVParser parser2 = new CSVParserBuilder().withSeparator(_inputDelimiters[_r]).build();
+                    String[] line2 = parser2.parseLine(l2);
+
+                    StringWriter buffer = new StringWriter();
+
+                    String[] acc = new String[_outputColumns.length];
+                    int i = 0;
+                    for (Tuple2<Integer, Integer> col : _outputColumns) {
+                        if (col._1 == _r) {
+                            acc[i] = line2[col._2];
+                        } else {
+                            acc[i] = line1[i];
+                        }
+
+                        i++;
+                    }
+
+                    CSVWriter writer = new CSVWriter(buffer, _outputDelimiter, CSVWriter.DEFAULT_QUOTE_CHARACTER,
+                            CSVWriter.DEFAULT_ESCAPE_CHARACTER, "");
+                    writer.writeNext(acc, false);
+                    writer.close();
+
+                    res.add(new Tuple2<>(o._1, new Text(buffer.toString())));
+                }
+
+                return res.iterator();
+            });
+        }
+
+        return Collections.singletonMap(outputName, j.values());
     }
 
     public enum Join implements Serializable {
